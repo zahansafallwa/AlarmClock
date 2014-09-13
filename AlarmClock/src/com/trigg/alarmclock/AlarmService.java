d7fd67bd44f3c393682d89dab3f300e6202d5ab3
zahansafallwa <safallwa@gmail.com>
2014-09-13 14:12:25 -0700

    Update AlarmService.java

--- AlarmClock/src/com/trigg/alarmclock/AlarmService.java
+++ AlarmClock/src/com/trigg/alarmclock/AlarmService.java
@@ -16,13 +16,14 @@ public class AlarmService extends Service {
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
 		
+		if(intent!=null){
 		Intent alarmIntent = new Intent(getBaseContext(), AlarmScreen.class);
 		alarmIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 		alarmIntent.putExtras(intent);
 		getApplication().startActivity(alarmIntent);
 		
 		AlarmManagerHelper.setAlarms(this);
-		
+		}
 		return super.onStartCommand(intent, flags, startId);
 	}
 	
